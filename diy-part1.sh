edd14c563f339fd25f636179660ffff6370ccf4c
homeassistad <129900034+homeassistad@users.noreply.github.com>
2023-04-24 00:54:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,6 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git luci-app-openclash openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+echo 'src-git luci-app-openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 echo 'src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >>feeds.conf.default
 
