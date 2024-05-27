e1aff5946d59ec1bf8a5a0bd6c6f2860700e278d
homeassistad <129900034+homeassistad@users.noreply.github.com>
2024-05-27 22:03:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,4 +15,4 @@
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci-nginx/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci-ssl-nginx/Makefile
-sed -i 's/192.168.1.1/192.168.1.53/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.30/g' package/base-files/files/bin/config_generate
