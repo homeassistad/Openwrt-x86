300bfad67595ccd8a905ef81baea2eb666b89f91
homeassistad <129900034+homeassistad@users.noreply.github.com>
2024-07-23 16:45:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,5 +18,6 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git luci-app-advancedplus https://github.com/sirpdboy/luci-app-advancedplus' >>feeds.conf.default
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
